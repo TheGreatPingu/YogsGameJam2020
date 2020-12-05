de1378ed5d1131d7a1e13a7d25518cdc5af48001
Daniel Sládeček <daniel.sladecek@student.tuke.sk>
2020-12-05 09:46:15 +0100

    Added aiming and camera movement

--- YogGameJam/Assets/Scripts/PlayerMovement.cs
+++ YogGameJam/Assets/Scripts/PlayerMovement.cs
@@ -6,17 +6,35 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rigidbody;
+    public Camera cam;
 
+    Vector2 currentPos;
     Vector2 movement;
+    Vector2 mousePos;
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        currentPos.x = transform.position.x;
+        currentPos.y = transform.position.y;
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
     {
+
+        //player sprite rotation
+        Vector2 lookDir = mousePos - rigidbody.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rigidbody.rotation = angle;
+
+        //player movement
         rigidbody.MovePosition(rigidbody.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        //cam movement
+        cam.transform.position = new Vector3(currentPos.x + lookDir.x/5, currentPos.y + lookDir.y/5, -10f);
     }
 }
