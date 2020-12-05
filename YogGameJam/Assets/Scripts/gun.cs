63990def0ba8832a5c930b84427684c3fa86bdfd
Daniel Sládeček <daniel.sladecek@student.tuke.sk>
2020-12-05 15:35:13 +0100

    gun and melee scripts added

---
+++ YogGameJam/Assets/Scripts/gun.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun : MonoBehaviour
+{
+
+    public Transform projectileSpawn;
+    public GameObject projectilePrefab;
+
+    public float force = 20f;
+
+    void Update()
+    {
+        /*
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Use();
+        }
+        */
+    }
+
+    void Use()
+    {
+
+        GameObject projectile = Instantiate(projectilePrefab, projectileSpawn.position, projectileSpawn.rotation);
+
+        Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+
+        rb.AddForce(projectileSpawn.up * force, ForceMode2D.Impulse);
+
+    }
+}
---
+++ YogGameJam/Assets/Scripts/melee.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class melee : MonoBehaviour
+{
+
+    public Transform meleeSpawn;
+    public GameObject meleePrefab;
+    public GameObject player;
+    public int swingSpeed = 400;
+    private GameObject meleeObj = null;
+
+    void Update()
+    {
+        
+        if (Input.GetButtonDown("Fire1") && meleeObj == null)
+        {
+            Use();
+        }
+        if (meleeObj != null)
+        {
+            meleeObj.transform.position = meleeSpawn.position;
+            meleeObj.transform.RotateAround(meleeSpawn.transform.position, Vector3.forward, swingSpeed * Time.deltaTime);
+            player.transform.Rotate(new Vector3(0, 0, player.transform.rotation.eulerAngles.z));
+        }
+        
+        
+    }
+
+    void Use()
+    {
+        meleeObj = Instantiate(meleePrefab, meleeSpawn.position, meleeSpawn.rotation);
+        meleeObj.transform.Rotate(new Vector3(0, 0, -40));
+        Destroy(meleeObj, 60f/swingSpeed);
+    }
+}
